a96aa9cfcd2eaf901eb6fa0aef19a4e15205dbb0
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2023-04-25 19:56:44 +0200

    Skaber en ny controller

---
+++ TT_Service/TT_API_Service/Controllers/ForemanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TT_DataAccess.Repository;
+
+namespace TT_API_Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ForemanController : Controller
+    {
+        // GET: ForemanController
+        [HttpGet(Name = "GetForeman")]
+        public string GetForeman()
+        {
+            ForemanRepository foremanRepo = new ForemanRepository();
+            //return View();
+            //return foremanRepo.GetForeman();
+            return "Foreman";
+        }
+
+    }
+}
---
+++ TT_Service/TT_DataAccess/Repository/ForemanRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TT_DataAccess.Repository
+{
+    public class ForemanRepository
+    {
+        public ForemanRepository() 
+        {
+            Initialize();
+        }
+        private void Initialize()
+        {
+            // Opret forbindelse til SQL Server
+            string connectionString = "Server=.\\SQLExpress;Database=FitterTest;IntegratedSecurity=true;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            // Åbn forbindelse til databasen
+            connection.Open();
+
+            // Udfør SQL-kommando
+            string sql = "SELECT TOP 1 FROM Fiters";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            //// Læs data fra databasen
+            //while (reader.Read())
+            //{
+            //    return reader["FirstName"].ToString();
+            //}
+
+            // Luk forbindelsen til databasen
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
