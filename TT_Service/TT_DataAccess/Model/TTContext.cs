592f18a46051393830fc07498af03fe166b0df9e
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2023-04-25 13:33:16 +0200

    Modeller

--- TT_Service/TT_DataAccess/Model/TTContext.cs
+++ TT_Service/TT_DataAccess/Model/TTContext.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace TT_DataAccess.Model
 {
-    internal class TTContext
+    public class TTContext
     {
+        private string _connectionString;
+
+        public TTContext()
+        {
+            Initialize();
+        }
+
+        private void Initialize() 
+        {
+            // Opret forbindelse til SQL Server
+            string connectionString = "Server=.\\SQLExpress;Database=FitterTest;IntegratedSecurity=true;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            // Åbn forbindelse til databasen
+            connection.Open();
+
+            // Udfør SQL-kommando
+            string sql = "SELECT * FROM Fiters";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Læs data fra databasen
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["FirstName"].ToString());
+            }
+
+            // Luk forbindelsen til databasen
+            reader.Close();
+            connection.Close();
+        } 
     }
 }
---
+++ TT_Service/TT_DataAccess/TestafDBcontext.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SqlClient;
+using TT_DataAccess.Model;
+//using TT_DataAccess.Model.TTContext;
+
+namespace TT_DataAccess
+{
+    public class TestafDBcontext
+    {
+        static void Main(string[] args)
+        {
+            //string connectionString = "Server=.\\SQLExpress;Database=FitterTest;Integrated Security=True;";
+            //SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                TTContext context = new TTContext();
+                //connection.Open();
+
+                //SqlCommand command = new SqlCommand("SELECT TOP 1 * FROM MyTable", connection);
+                //SqlDataReader reader = command.ExecuteReader();
+
+                //while (reader.Read())
+                //{
+                //    Console.WriteLine(reader[0] + "\t" + reader[1] + "\t" + reader[2]);
+                //}
+                //reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                //connection.Close();
+                Console.WriteLine("All done");
+            }
+
+        }
+    }
+}
